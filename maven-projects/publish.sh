8c063aaaee478db643a4e08cf66d41d98f7e2b09
Anthony Sceresini <asceresini@gmail.com>
2016-09-07 22:00:01 +1000

    fix versioning of hello-world-api

--- maven-projects/publish.sh
+++ maven-projects/publish.sh
@@ -5,5 +5,7 @@ set -euf -o pipefail
 # this is run from within a source code directory so path references will
 # reflect that
 
-mvn $CMD
+VERSION=$(cat ../version/number)
+mvn versions:set -DnewVersion=${VERSION}
+mvn deploy
 
