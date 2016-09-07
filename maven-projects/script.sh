6e13dd5ea2ccf0aecda364d03008b1b6dff4e7d1
Anthony Sceresini <asceresini@gmail.com>
2016-09-07 21:44:13 +1000

    adding versioning and deploying to repo

--- maven-projects/script.sh
+++ maven-projects/script.sh
@@ -5,5 +5,9 @@ set -euf -o pipefail
 # this is run from within a source code directory so path references will
 # reflect that
 
+if [[ -n "${VERSION}" ]]; then
+  mvn versions:set -DnewVersion=${VERSION}
+fi
+
 mvn $CMD
 
